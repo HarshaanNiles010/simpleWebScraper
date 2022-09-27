9ad6d2f12b41d139ccdcaf88e1a7f127c25af03f
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-09-27 17:54:28 -0600

    priceChecker

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-
+import time
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 # driver.get("https://www.footlocker.ca/en/release-dates")
@@ -14,8 +14,14 @@ try:
     driver.get("https://www.footlocker.ca/en/release-dates")
     productPage = driver.find_element(By.CLASS_NAME, "c-release-calender-details")
     products = productPage.find_elements(By.CLASS_NAME, "ReleaseCalendar-Products")
+    results = []
     for product in products:
         info = product.find_elements(By.CLASS_NAME,"col")
-        print(info)
+        results.append([i.text for i in info])
+    print(results)
 except:
+    print("Encountered some problem closing the web page")
+    driver.quit()
+finally:
+    time.sleep(5)
     driver.quit()
