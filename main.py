a70f8d06787495719bc6d21d71be99c6811cd3eb
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-09-27 18:29:13 -0600

    cardChecker

--- main.py
+++ main.py
@@ -3,25 +3,43 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 import time
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-# driver.get("https://www.footlocker.ca/en/release-dates")
-# #col
-# products = driver.find_element(By.CLASS_NAME,"c-release-calender-details")
-# print(products.text)
-# driver.quit()
-try:
-    driver.get("https://www.footlocker.ca/en/release-dates")
-    productPage = driver.find_element(By.CLASS_NAME, "c-release-calender-details")
-    products = productPage.find_elements(By.CLASS_NAME, "ReleaseCalendar-Products")
-    results = []
-    for product in products:
-        info = product.find_elements(By.CLASS_NAME,"col")
-        results.append([i.text for i in info])
-    print(results)
-except:
-    print("Encountered some problem closing the web page")
-    driver.quit()
-finally:
-    time.sleep(5)
-    driver.quit()
+
+
+def siteScraper():
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+    try:
+        driver.get("https://www.footlocker.ca/en/release-dates")
+        productPage = driver.find_element(By.CLASS_NAME, "c-release-calender-details")
+        products = productPage.find_elements(By.CLASS_NAME, "ReleaseCalendar-Products")
+        results = []
+        for product in products:
+            info = product.find_elements(By.CLASS_NAME, "col")
+            results.append([i.text for i in info])
+        print(results)
+    except:
+        print("Encountered some problem closing the web page")
+        driver.quit()
+    finally:
+        time.sleep(5)
+        driver.quit()
+
+
+cardPath = r"C:\Users\harshaan\PycharmProjects\pythonProject1\cards.txt"
+
+
+# making sure the card entered will actually work
+def cardVerify(cardPath):
+    card = []
+    try:
+        f = open(cardPath, "r", encoding='utf-8')
+        card.append(f.readline())
+        print(card)
+        f.close()
+    except:
+        print("Process failed")
+    finally:
+        return
+
+
+cardVerify(cardPath)
