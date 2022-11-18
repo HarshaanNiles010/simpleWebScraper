8ee7c9709c7e1684d82807fb43cef744fd694f67
HarshaanNiles010 <harshaanniles010@gmail.com>
2022-11-17 19:09:27 -0800

    new Scraper Design

--- main.py
+++ main.py
@@ -1,67 +1,56 @@
+import csv
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-import time
+from bs4 import BeautifulSoup
+import requests
 
-from selenium.webdriver.support.wait import WebDriverWait
-
-
-def siteScraper():
-    PATH = "C:\Program Files (x86)\chromedriver.exe"
-    driver = webdriver.Chrome(PATH)
-    try:
-        driver.get("https://www.footlocker.ca/en/category/new-arrivals.html")
-        driver.find_element(By.CLASS_NAME, "ProductCard").click()
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "Buttons--stackOnMobile"))
-        )
-        # products = driver.find_element(By.CLASS_NAME,"ProductDetails-form__info")
-        driver.find_element(By.CSS_SELECTOR, "[aria-label='Size: 09.0']").click()
-        element = WebDriverWait(driver, 10).until(
-             EC.element_to_be_clickable(
-                 (By.CLASS_NAME,"Buttons--stackOnMobile"))
-        )
-        #driver.find_element(By.PARTIAL_LINK_TEXT,"Add To Cart").click()
-
-        # driver.find_element(By.XPATH,"//button[@class='Button ProductDetails-form__action'][.='Add To Cart']").click()
-        # print(products.text)
-        # print(products.text)
-
-        # productPage = driver.find_element(By.CLASS_NAME, "SearchResults")
-        # product = productPage.find_element(By.CLASS_NAME,"ProductCard")
-        # product.click()
-        # wait = WebDriverWait(driver, 10)
-        # element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'Buttons--stackOnMobile')))
-        # ids = product.find_elements(By.ID,"main")
-        # print(ids)
-        # cart = driver.find_element(By.CLASS_NAME,"Button Button--alt")
+URL = "https://www.footlocker.ca/en/category/new-arrivals.html"
+r = requests.get(URL)
+soup = BeautifulSoup(r.content, 'html5lib')
+sales = soup.find('div', attrs={'class': 'SearchResults'})
+itemLinks = []
+counter = 0
+for row in sales.find_all('li', attrs={"class": "product-container col"}):
+    reading = {}
+    # reading['descr'] = row.img['alt']
+    reading['link' + str(counter)] = row.a['href']
+    itemLinks.append(reading)
+    counter += 1
 
 
-    except Exception as e:
-        print(f'error detail {e}')
+# print(ShowData)
+# fileName = 'new_data.csv'
+# counter = 1
+# with open(fileName,'w') as f:
+#     w = csv.DictWriter(f,['descr','link'])
+#     counter += 1
+#     w.writeheader()
+#     for data in ShowData:
+#         w.writerow(data)
 
-    finally:
-        time.sleep(5)
-        driver.quit()
-
-
-cardPath = r"C:\Users\harshaan\PycharmProjects\pythonProject1\cards.txt"
-
-
-# making sure the card entered will actually work
-def cardVerify(cardPath):
-    card = []
-    try:
-        f = open(cardPath, "r", encoding='utf-8')
-        card.append(f.readline())
-        print(card)
-        f.close()
-    except:
-        print("Process failed")
-    finally:
-        return
+def urlProcessing(URL1, URL2):
+    URL1 = list(URL.split("/"))
+    URL1 = URL1[:3]
+    URL1 = "/".join(URL1)
+    newURL = URL1 + URL2
+    return newURL
 
 
-# cardVerify(cardPath)
-siteScraper()
+def linkMaker(itemLinks):
+    counter = 0
+    itemURLS = []
+    for links in itemLinks:
+        itemURLS.append(urlProcessing(URL, links['link' + str(counter)]))
+        counter += 1
+
+    return itemURLS
+
+
+def OpenLinks(itemURLS):
+    driver = webdriver.Chrome()
+    for urls in itemURLS:
+        driver.get(urls)
+
+    driver.close()
+
+
+OpenLinks(linkMaker(itemLinks))
